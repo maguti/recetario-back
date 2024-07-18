b096283ed1c1f0769c46f898bfafece67fad53f5
Miguel <gma_m@hotmail.com>
2024-07-18 09:09:28 -0300

    Fix readme

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ pip install PyMySQL
 Ejecutar script sql: "recetario2.sql" para crear la base de datos y las tablas y datos iniciales
 
 Link publicación website Proyecto II (Backend):
-
+https://maguti.pythonanywhere.com/
 
 Link publicación website Proyecto I (Frontend):
 https://recetarioweb.netlify.app/
--- db.py
+++ db.py
@@ -7,15 +7,15 @@ def conectarMySQL():
     # datos sensibles -> variables de entorno
     
     # local
-    # host="localhost"
-    # user="root"
-    # password="root"
-    # db="recetario"
+    host="localhost"
+    user="root"
+    password="root"
+    db="recetario"
     
     # deploy -> Pythonanywhere
-    host="maguti.mysql.pythonanywhere-services.com"
-    user="maguti"
-    password="codo2024"
-    db="maguti$recetario"
+    # host="maguti.mysql.pythonanywhere-services.com"
+    # user="maguti"
+    # password="codo2024"
+    # db="maguti$recetario"
 
     return pymysql.connect(host=host,user=user,password=password,database=db)
